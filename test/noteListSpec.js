9df69df17ac273a79a2307154bc6ad60300fd411
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-28 14:53:58 +0100

    Fix notelistspec equality matching

--- test/noteListSpec.js
+++ test/noteListSpec.js
@@ -1,7 +1,7 @@
 function testAddNotestoNotesArray(){
 
   NoteList.createNote("This is one of the notes to add");
-  assert.isTrue(NoteList.allNotes().length === 2);
+  assert.isTrue(NoteList.allNotes().length === 1);
 }
 
 testAddNotestoNotesArray();
