5f4ff565cd8efa22b015744f236f4537b8835ac3
Ashwini <ashie.oh@gmail.com>
2017-03-28 18:14:33 +0100

    Adds onclick functionality for ul

--- public/javascript/noteListSaveUserContent.js
+++ public/javascript/noteListSaveUserContent.js
@@ -2,14 +2,37 @@ var notelist;
 
 window.onload = function() {
   notelist = new NoteList();
+  notelistview = new NoteListView(notelist);
 }
 
+function getIt(id){
+  return document.getElementById(id);
+}
 
 function noteListSaveUserContent() {
-  notelist.createNote(document.getElementById('note-text').value);
-  document.getElementById('note-text').value = '';
+  notelist.createNote(getIt('note-text').value);
+  getIt('note-text').value = '';
+  getIt('list').innerHTML = notelistview.displaySummary();
 }
 
-document.getElementById("submit-btn").addEventListener("click", function() {
+getIt("submit-btn").addEventListener("click", function() {
   noteListSaveUserContent();
 });
+
+getIt('list').addEventListener("click", function() {
+  children = getIt('list').childNodes;
+});
+
+
+function getEventTarget(e) {
+    e = e || window.event;
+    return e.target || e.srcElement;
+}
+
+var ul = getIt('list');
+ul.onclick = function(event) {
+    var target = getEventTarget(event);
+index = target.id;
+note = notelist.allNotes()[index];
+getIt('full_content').innerHTML = note.seeFullContent();
+};
