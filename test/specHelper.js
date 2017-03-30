2fabc372f9f6e91a3a44a346a6286fd1cc90a2cd
Vicky Roberts <vicki_uk@hotmail.com>
2017-03-30 13:57:27 +0100

    Insert test assertions to specRunner, plus test styles

--- test/specHelper.js
+++ test/specHelper.js
@@ -1,40 +1,48 @@
 var assert = {
 
-  testMessage: function(text) {
+  testMessage: function(text, color) {
       li = document.createElement("li");
       li.innerHTML = text;
+      li.style.color = color;
       document.getElementById('tests').appendChild(li);
   },
 
   isEqual: function(actual, expected) {
-    var text;
     if (actual !== expected) {
-      text = "\nAssertion failed:\n" + actual + "\n**IS NOT EQUAL TO**\n" + expected;
+      text = "\nASSERTION FAILED:\n" + actual + "\n**IS NOT EQUAL TO**\n" + expected;
+      color = "red";
     }
     else {
       text = "TEST PASSED!\n" + actual + "\n**is equal to**\n" + expected;
+      color = "green";
     }
-    this.testMessage(text);
+    this.testMessage(text, color);
   },
 
   isTrue: function(assertionToCheck) {
     if(!assertionToCheck){
-      text = "\nAssertion failed:\n" + assertionToCheck + "\n**is not truthy.**";
+      text = "\ASSERTION FAILED:\n" + assertionToCheck + "\n**is not truthy.**";
+      color = "red"
     }
     else {
       text = "TEST PASSED!\n" + assertionToCheck + "\n**is truthy**";
+      color = "green";
+
     }
-    this.testMessage(text);
+    this.testMessage(text, color);
   },
 
   isInstanceOf: function(object, Constructor){
     if(object.constructor !== Constructor){
-      text = "\nComparison failed:\n" + object + "\n**is not an instance of.**" + Constructor;
+      text = "\nCOMPARISON FAILED:\n" + object + "\n**is not an instance of.**" + Constructor;
+      color = "red";
     }
     else {
       text = "TEST PASSED!\n" + object + "\n**is an instance of**" + Constructor;
+      color = "green";
+
     }
-    this.testMessage(text);
+    this.testMessage(text, color);
   },
 
   createMock: function(name, method, returns){
