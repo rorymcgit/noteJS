456b48b72553d2ce175e5f8ad60007a7b98ebb16
Ashwini <ashie.oh@gmail.com>
2017-04-01 18:24:05 +0100

    Adds a missing semicolon

--- test/noteSpec.js
+++ test/noteSpec.js
@@ -1,6 +1,6 @@
 
 function testSeeFullContent() {
-  var expected = "Birthday Notes here, its my birthday in a few months, give me lots of presents"
+  var expected = "Birthday Notes here, its my birthday in a few months, give me lots of presents";
   var note = new Note(expected);
   var actual = note.seeFullContent();
   assert.isEqual(actual, expected);
